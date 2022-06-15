6628f4d42be0701a495e5ca9680a636bba28ad9f
Pushpendra Kumar <kumarpushp1992@gmail.com>
2022-06-16 00:16:33 +0530

    restaurent part1 completed

--- reataurent_details/src/Components/Restaurents.jsx
+++ reataurent_details/src/Components/Restaurents.jsx
@@ -1,5 +1,16 @@
 
 export default function Restaurent({res}){
+    const cate= (cat)=>{
+        let s = "";
+        cat.forEach(e=>{
+            s = s+" "+e;
+        })
+        return s
+    };
+
+    const ratings=(x)=>{
+        return x/5
+    }
     return(
         <div>
             <div>
@@ -10,7 +21,11 @@ export default function Restaurent({res}){
                     <h1>{res.name}</h1>
                     <h3>{cate(res.categories)}</h3>
                     <h3>{`Rs ${res.cost_of_one}`}</h3>
-                    <h3>{`Accepts ${res.payment_methods.cash??"cash"}, ${res.payment_methods.card??"card"}, ${res.payment_methods.cash??"cash"} only`}</h3>
+                    <h3>{`Accepts ${res.payment_methods.cash?"cash":""}, ${res.payment_methods.card?"card":""}, ${res.payment_methods.cash?"online":""} payments only`}</h3>
+                </div>
+                <div>
+                    <h2>{res.ratig}</h2>
+                    <h4>{`${res.total_votes} votes`}</h4>
                 </div>
             </div>
             <div></div>
